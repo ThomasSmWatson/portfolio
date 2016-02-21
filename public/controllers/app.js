a3d060d56a4d169e40f77ec0bbd0070442444bad
Thomas Watson <thomas.sm.watson@gmail.com>
2016-02-21 02:54:25 +0000

    create initial structure

---
+++ public/controllers/app.js
@@ -0,0 +1,8 @@
+var app = angular.module("app",[]);
+
+// app.config(['$routeProvider',function($routeProvider){
+
+// }]);
+app.controller('HomeController',["$scope",function($scope){
+	console.log("Holla!");
+}]);
