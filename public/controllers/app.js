ae8636e976893b36b5f8fa53352f72c2f3caeed6
Thomas Watson <thomas.sm.watson@gmail.com>
2016-02-22 03:58:58 +0000

    Add views to each section

--- public/controllers/app.js
+++ public/controllers/app.js
@@ -4,7 +4,19 @@ app.config(['$routeProvider',function($routeProvider){
 	$routeProvider.when('/',{
 		templateUrl:'../views/home.html',
 		controller: 'HomeController'
+	}).
+	when('/about',{
+		templateUrl:'../views/about.html'
+	}).
+	when('/portfolio',{
+		templateUrl:'../views/portfolio.html'
 	})
+	.when('/contact',{
+		templateUrl:'../views/contact.html'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
 }]);
 app.controller('HomeController',["$scope",function($scope){
 	
