83b630e59322d0161b908f089739ada7064b6c6b
Thomas Watson <thomas.sm.watson@gmail.com>
2016-02-23 04:49:33 +0000

    cleanup in-line css into site.css, add missing about controller (forgot to commit.. oops!)

---
+++ public/controllers/about.js
@@ -0,0 +1,13 @@
+var app = angular.module("about",[]);
+
+app.controller('AboutController',["$scope",function($scope){
+	$scope.consultant = 
+		{
+			showDetail: false,
+			img_src: "https://scontent-lhr3-1.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/12108078_10205023476378668_2351755957653627514_n.jpg?oh=7b999f4a22718125b6d0b77c00c203ad&oe=5757F595",
+			name: "Thomas Watson",
+			description: "Hello! It's a pleasure for you to visit my website! I have been programming for 4 years now (since i have been 15) and have managed to persue what started as a fun hoby, into a cereer.",
+			hobbies: ["Web Development", "Swimming", "Scripting/Automating things", "Martial Arts"]
+		}
+	
+}]);
